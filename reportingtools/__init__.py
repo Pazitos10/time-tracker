d80a98b24e233c8110d9f74784cf25dd5ec692cb
max-torch <lacsonalexanderz@gmail.com>
2021-07-25 18:56:55 +0800

    Update __init__.py
    Add docstring

---
+++ reportingtools/__init__.py
@@ -0,0 +1,8 @@
+"""Scripts and assets for producing summaries and reports of the app data.
+
+Scripts:
+
+    reportutils: Converts start and end columns to datetime format and adds an 'Hours Worked' column
+    containing a Timedelta of each session.
+    
+"""
